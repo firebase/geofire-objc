0c7e3eec7b12df33a97c4676c508053cc06da492
Jonny Dimond <jonny@firebase.com>
2014-07-03 11:54:55 +0200

    Initial Commit

---
+++ GeoFire/GeoFire.h
@@ -0,0 +1,13 @@
+//
+//  GeoFire.h
+//  GeoFire
+//
+//  Created by Jonny Dimond on 7/3/14.
+//  Copyright (c) 2014 Firebase. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface GeoFire : NSObject
+
+@end
---
+++ GeoFire/GeoFire.m
@@ -0,0 +1,13 @@
+//
+//  GeoFire.m
+//  GeoFire
+//
+//  Created by Jonny Dimond on 7/3/14.
+//  Copyright (c) 2014 Firebase. All rights reserved.
+//
+
+#import "GeoFire.h"
+
+@implementation GeoFire
+
+@end
