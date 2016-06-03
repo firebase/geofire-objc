06fa95394638d5b6cd41550b81cdfe4297eea75b
Tim Kreger <Tim.Kreger@intelematics.com>
2016-06-03 16:36:27 +1000

    Manual configure of app for testing.

--- GeoFireTests/GFRealDataTest.m
+++ GeoFireTests/GFRealDataTest.m
@@ -10,22 +10,35 @@
 
 #import "TestHelpers.h"
 
+@import Firebase;
+
 @implementation GFRealDataTest
 
 - (void)setUp
 {
     [super setUp];
-
-    static dispatch_once_t onceToken;
-    dispatch_once(&onceToken, ^{
-         [FIRApp configure];
-    });
-
     NSString *randomFirebaseURL = [NSString stringWithFormat:@"https://%@.firebaseio-demo.com",
-                                [TestHelpers randomAlphaNumericStringWithLength:16]];
+                                   [TestHelpers randomAlphaNumericStringWithLength:16]];
+
+    NSString *appname =  [TestHelpers randomAlphaNumericStringWithLength:16];
+    NSLog(@"Test url %@", randomFirebaseURL);
+    FIROptions *options = [[FIROptions alloc] initWithGoogleAppID:@"googleAPI"
+                                                 bundleID:@"com.test.app"
+                                            GCMSenderID:@"testid"
+                                                   APIKey:nil
+                                                clientID:nil
+                                               trackingID:nil
+                                          androidClientID:nil
+                                              databaseURL:randomFirebaseURL
+                                            storageBucket:nil
+                                        deepLinkURLScheme:nil];
+
+
+    [FIRApp configureWithName:appname options:options];
+
     dispatch_queue_t backgroundQueue = dispatch_queue_create("com.firebase.test", NULL);
-   [[FIRDatabase database]  setCallbackQueue:backgroundQueue];
-    self.firebaseRef = [[FIRDatabase database] referenceFromURL:randomFirebaseURL];
+    [[FIRDatabase databaseForApp:[FIRApp appNamed:appname]]  setCallbackQueue:backgroundQueue];
+    self.firebaseRef = [[FIRDatabase databaseForApp:[FIRApp appNamed:appname]] reference];
     self.geoFire = [[GeoFire alloc] initWithFirebaseRef:self.firebaseRef];
     self.geoFire.callbackQueue = backgroundQueue;
 }
@@ -34,9 +47,11 @@
 {
     [super tearDown];
     dispatch_semaphore_t wait = dispatch_semaphore_create(0);
+
     [self.firebaseRef setValue:nil withCompletionBlock:^(NSError *error, FIRDatabaseReference *ref) {
         dispatch_semaphore_signal(wait);
     }];
+
     dispatch_semaphore_wait(wait, dispatch_time(DISPATCH_TIME_NOW, 2 * NSEC_PER_SEC));
 }
 
