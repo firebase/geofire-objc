298e03d32c7a2e6e4f8225399d5d8111f475e821
J.J. Riemersma <j.j.riemersma@student.rug.nl>
2021-02-06 19:43:18 +0100

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -1,7 +1,11 @@
+// swift-tools-version:5.3
 import PackageDescription
 
 let package = Package(
     name: "geofire_objc",
+    platforms: [
+        .iOS(.v14)
+    ],
     products: [
         .library(name: "geofire_objc", targets: ["geofire_objc"])
     ],
