0a65f64e0139bf1d2d7058eda569784d2bb131b6
J.J. Riemersma <j.j.riemersma@student.rug.nl>
2021-02-06 19:36:57 +0100

    Create Package.swift

---
+++ Package.swift
@@ -0,0 +1,13 @@
+import PackageDescription
+
+let package = Package(
+    name: "geofire_objc",
+    products: [
+        .library(name: "geofire_objc", targets: ["geofire_objc"])
+    ],
+    targets: [
+        .target(
+            name: "GeoFire",
+            path: "GeoFire",
+    ]
+)
